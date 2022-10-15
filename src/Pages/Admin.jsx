7bf7ad111fcf973d0e9f52310504261739867ab8
zebbyjz <megajehanzaib@gmail.com>
2022-10-16 01:07:34 +0300

    -Extract image as base64. -get all form fields

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,17 +1,127 @@
 import React, { Component } from "react";
-import {Formik} from 'formik';
-import logo from '../dphi_logo.svg'
-
+import { Formik } from "formik";
+import logo from "../dphi_logo.svg";
 
 class Admin extends Component {
   state = {};
+
+  input_schema = [
+    {
+      id: 1,
+      name: "Challenge Name",
+      type: "text",
+      placeholder: "Challenge Name",
+      value: "",
+      label: "Challenge Name",
+    },
+
+    {
+      id: 2,
+      name: "Start date",
+      type: "date",
+      placeholder: "Add Start Date",
+      value: "",
+      label: "Start Date",
+    },
+
+    {
+      id: 3,
+      name: "Start date",
+      type: "date",
+      placeholder: "Add End Date",
+      value: "",
+      label: "End Date",
+    },
+  ];
+
+  onSubmit = (event) => {
+    const formData = new FormData(event.currentTarget);
+    event.preventDefault();
+    let packed_array = [];
+    let base64String="";
+
+    for (const field of formData) {
+      if (field[0] === "img-upload" && field[1]) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          console.log("Full url", reader.result);
+          base64String = reader.result
+            .replace("data:", "")
+            .replace(/^.+,/, "");
+            let obj = {};
+        obj[field[0]] = base64String;
+        packed_array.push(obj);
+          console.log("BASE64", base64String);
+        };
+        reader.readAsDataURL(field[1]);
+      }
+      else {
+        let obj = {};
+        obj[field[0]] = field[1];
+        packed_array.push(obj);
+      }
+    }
+
+    console.log(packed_array)
+    
+    }
+
+
 
   render() {
     return (
       <React.Fragment>
         <img src={logo} alt="DPhi"></img>
-        <h2 className="mt-3">Challenge details</h2>
-        
+        <h2 className="mt-3 mb-5">Challenge details</h2>
+        <div className="form-group row">
+          <div className="col-sm-4">
+            <form onSubmit={this.onSubmit}>
+              {this.input_schema.map((_input) => {
+                return (
+                  <React.Fragment>
+                    <label htmlFor={_input.id}>{_input.label}</label>
+                    <input
+                      key={_input.id}
+                      id={_input.id}
+                      type={_input.type}
+                      name={_input.name}
+                      placeholder={_input.placeholder}
+                      className="form-control mb-3"
+                    ></input>
+                  </React.Fragment>
+                );
+              })}
+              <label htmlFor="desc" className="mt-3">
+                Description
+              </label>
+              <textarea
+                name="description"
+                id="desc"
+                className="form-control"
+                rows="10"
+              ></textarea>
+
+              <label className="mt-3">Image</label>
+              <input name="img-upload" type="file" className="form-control" />
+
+              <label className="mt-3">Difficulty</label>
+
+              <select
+                name="difficulty"
+                className="form-select"
+                aria-label="Difficulty"
+              >
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+              </select>
+
+              <button type="submit" className="btn btn-success mt-4">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Admin from './Pages/Admin';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='container'>
       <App></App>
-      <Admin></Admin>
+      
     </div>
   </React.StrictMode>
 );
